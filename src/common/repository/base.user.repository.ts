0b4d2e392c7d3a981b0356103e158908a1a6b566
Alireza <alirezax69prom@gmail.com>
2023-08-13 20:44:30 +0330

    refactor: add a base class for user repository
    To avoid repetition, the base class has functions that are often used.

---
+++ src/common/repository/base.user.repository.ts
@@ -0,0 +1,32 @@
+import { Entity } from '../constant';
+import { Repository } from 'typeorm';
+import { Injectable, Inject } from '@nestjs/common';
+import { UserEntity } from 'src/db/entities';
+
+@Injectable()
+export abstract class BaseUserRepository {
+  constructor(@Inject(Entity.Users) protected user: Repository<UserEntity>) {}
+
+  async findById(userId: string, includePassword = false) {
+    return await this.findBy({ id: userId }, includePassword);
+  }
+
+  async findByEmail(email: string, includePassword = false) {
+    return await this.findBy({ email: email }, includePassword);
+  }
+
+  private async findBy(query: Record<string, any>, includePassword: boolean) {
+    return await this.user.findOne({
+      where: query,
+      select: {
+        id: true,
+        avatars: true,
+        createdAt: true,
+        email: true,
+        bio: true,
+        name: { fname: true, lname: true, nickname: true },
+        password: includePassword,
+      },
+    });
+  }
+}
